c8161600b5bb320fdc2ea170f11ff66cef28de69
Tom Graßmann <tom.grassmann@live.de>
2016-07-05 17:08:38 +0200

    State Features added.

--- src/GarageDoor.java
+++ src/GarageDoor.java
@@ -22,24 +22,109 @@ public class GarageDoor {
         this.currentState = currentState;
     }
 
-    public void openDoor(){
+    public void openDoor() throws IllegalStateException{
 
+        if (this.currentState.equals(new Open()) || this.currentState.equals(new Opening())){
+            throw new IllegalStateException("State have to be 'Closing' or 'Closed'!");
+        }
+
+        this.setState(new Opening());
+        this.motor.upwards();
     }
 
-    public void stopper(){
+    public void stopper() throws IllegalStateException{
 
+        if (this.currentState.equals(new Closed()) || this.currentState.equals(new Open())){
+            throw new IllegalStateException("Door has to be in state 'opening' or 'closing'!");
+        }
+
+        if (this.currentState.equals(new Opening())){
+
+            this.motor.stop();
+            this.setState(new Open());
+        }
+        else if (this.currentState.equals(new Closing())){
+
+            this.motor.stop();
+            this.setState(new Closed());
+        }
     }
 
-    public void closeDoor(){
+    public void closeDoor() throws IllegalStateException{
 
+        if (this.currentState.equals(new Closed()) || this.currentState.equals(new Closing())){
+            throw new IllegalStateException("State have to be 'Opening' or 'Open'!");
+        }
+
+        this.setState(new Closing());
+        this.motor.downwards();
     }
 
     public Motor getMotor(){
 
-        return null;
+        return this.motor;
     }
 
-    private void setState(DoorState ds){
+    private void setState(DoorState ds) throws NullPointerException{
 
+        if (ds == null){
+            throw new NullPointerException("DoorState cannot be NULL!");
+        }
+
+        this.currentState = ds;
+    }
+
+    // INNER CLASSES //
+    private class Open extends DoorState {
+
+        public Open(){
+
+        }
+
+        public void closeDoor(){
+
+        }
+    }
+
+    private class Opening extends DoorState {
+
+        public Opening(){
+
+        }
+
+        public void openDoor(){
+
+        }
+
+        public void stopper(){
+
+        }
+    }
+
+    private class Closed extends DoorState {
+
+        public Closed(){
+
+        }
+
+        public void openDoor(){
+
+        }
+
+    }
+
+    private class Closing extends DoorState {
+
+        public Closing(){
+
+        }
+
+        public void closeDoor(){
+
+        }
+
+        public void stopper(){
+
+        }
     }
 }
